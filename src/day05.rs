b44b42a238f17a638c747fd027c51a4214782731
Ryder McMinn <mcminnra@gmail.com>
2023-12-05 02:26:06 -0800

    more day05 work

--- src/day05.rs
+++ src/day05.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::fs;
-use std::iter::zip;
+use std::ops::Range;
+use std::cmp;
 
 #[derive(Debug)]
 struct AlmanacItem {
@@ -9,6 +10,17 @@ struct AlmanacItem {
     dest: String,
     dest_num: usize,
     range: usize,
+}
+
+fn is_overlap(a: Range<usize>, b: Range<usize>) -> bool {
+    a.start < b.end && b.start < a.end
+}
+
+fn get_overlap(a: Range<usize>, b: Range<usize>) -> Range<usize> {
+    let lower = cmp::max(a.start, b.start) as usize;
+    let upper = cmp::min(a.end, b.end) as usize;
+
+    lower..upper
 }
 
 fn main() {
@@ -101,7 +113,7 @@ fn main() {
     // Part 2
     // Create almanac mapping
     // TODO: research data structs
-    let mut almanac: HashMap<(String, usize), (String, usize)> = HashMap::new();
+    let mut almanac: HashMap<(String, Range<usize>), (String, Range<usize>)> = HashMap::new();
     let mut current_source = String::new();
     let mut current_dest = String::new();
     println!("Creating almanac HashMap...");
@@ -131,28 +143,91 @@ fn main() {
             let dest = current_dest.clone();
             let dest_num = mappings.get(0).unwrap().to_owned(); // NOTE: dest first
             let range = mappings.get(2).unwrap().to_owned();
-
-            for (s, d) in zip(source_num..source_num+range, dest_num..dest_num+range) {
-                almanac.insert((source.to_owned(), s), (dest.to_owned(), d));
-            }
+            almanac.insert(
+                (source.to_owned(), source_num..source_num + range),
+                (dest.to_owned(), dest_num..dest_num + range),
+            );
         }
     }
     println!("Creating almanac HashMap...DONE!");
 
-    let mut new_seeds: Vec<usize> = vec![];
-    for pair in seeds.windows(2) {
-        println!("Checking {:?}", pair);
-        // Find overlaping seed nums for seed ranges and seed-to-soil maps
-        let (seed_num, seed_range) = (pair.get(0).unwrap().clone(), pair.get(1).unwrap().clone());
-        for num in seed_num..seed_num+seed_range {
-            if almanac.contains_key(&("seed".to_string(), num)) {
-                new_seeds.push(num);
+    let sources_chain = vec![
+        "seed",
+        "soil",
+        "fertilizer",
+        "water",
+        "light",
+        "temperature",
+        "humidity",
+    ];
+
+    let mut source_pairs: Vec<(String, Range<usize>)> = seeds
+        .windows(2)
+        .map(|pair| {
+            (pair.get(0).unwrap().clone(), pair.get(1).unwrap().clone())
+        })
+        .map(|(start, range)| {
+            ("seed".to_string(), (start..start+range))
+        })
+        .collect();
+
+    let mut locations: Vec<usize> = vec![];
+    while !source_pairs.is_empty() {
+        let (current_source, current_range) = source_pairs.pop().unwrap();
+        //dbg!((&current_source, &current_range));
+
+        let keys: Vec<_> = almanac
+            .keys()
+            .filter(|(key_source, key_source_range)| {
+                *key_source == current_source && is_overlap(current_range.clone(), key_source_range.clone())
+            })
+            .collect();
+
+        if keys.is_empty() {
+            let source_idx = sources_chain.iter().position(|&s| s == current_source).unwrap();
+            if source_idx != sources_chain.len()-1 {
+                source_pairs.push((sources_chain.get(source_idx+1).unwrap().to_string(), current_range.clone()));
+            }
+        }
+
+        for key in keys {
+            let (key_source, key_source_range) = key;
+            let (key_dest, key_dest_range) = almanac.get(&key).unwrap();
+
+            let source_overlap = get_overlap(current_range.clone(), key_source_range.clone());
+            
+            // Put prefixes and suffixes back in queue
+            if current_range.start != source_overlap.start {
+                let source_prefix = current_range.start..source_overlap.start;
+                let source_pair = (key_source.to_owned(), source_prefix);
+                if !source_pairs.contains(&source_pair){
+                    source_pairs.push(source_pair);
+                }
+            }
+            if source_overlap.end != current_range.end {
+                let source_suffix = source_overlap.end..current_range.end;
+                let source_pair = (key_source.to_owned(), source_suffix);
+                if !source_pairs.contains(&source_pair){
+                    source_pairs.push(source_pair);
+                }
+            }
+            
+            // Convert to dest
+            let start_delta = source_overlap.start-key_source_range.start;
+            let end_delta = key_source_range.end-source_overlap.end;
+            let dest_overlap = (key_dest_range.start+start_delta)..(key_dest_range.end-end_delta);
+
+            // Processes overlap
+            if key_dest == "location" {
+                locations.push(dest_overlap.start);
+                //dbg!(source_pairs.len());
+                //dbg!(locations.iter().min().unwrap());
+            } else {
+                source_pairs.push((key_dest.to_owned(), dest_overlap));
             }
         }
     }
-    dbg!(new_seeds.len());
-
-
+    println!("Lowest Location from Seed Ranges: {}", locations.iter().min().unwrap());
 
     // == Old Brute Force
     // // // Maybe Cheating - Just get all seed-to-soil nums to start, no need to check all ranges if we don't have a map for it.
